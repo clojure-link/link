6c75a9b283f087646fba41a7bc9ea60ed804088c
Sunng <classicning@gmail.com>
2012-02-28 14:28:51 +0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject link "0.1.0-SNASHOT"
+  :description "A straightforward clojure wrapper for java nio framework"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [io.netty/netty "3.3.1.Final"]])
+
---
+++ src/link/core.clj
@@ -0,0 +1,1 @@
+(ns link.core)
