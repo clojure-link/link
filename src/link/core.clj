3693e55384edfe23c77cfdb132aa00dd0f4db936
Sunng <classicning@gmail.com>
2012-02-28 15:09:10 +0800

    add macro for defhandler

--- src/link/core.clj
+++ src/link/core.clj
@@ -1,1 +1,21 @@
 (ns link.core)
+
+(defmacro ^{:private true} make-handler-macro [evt]
+  (let [handler-name (str "on-" evt)
+        symbol-name (symbol handler-name)
+        args-vec-sym (symbol "args-vec")
+        body-sym (symbol "body")]
+    `(defmacro ~symbol-name [~args-vec-sym & ~body-sym]
+       `{(keyword ~~handler-name) (fn ~~args-vec-sym ~@~body-sym)})))
+
+(make-handler-macro open)
+(make-handler-macro close)
+(make-handler-macro message)
+(make-handler-macro error)
+(make-handler-macro connect)
+(make-handler-macro disconnect)
+
+(defmacro defhandler [n & body]
+  `(def ~n (merge ~@body)))
+
+
