8f3e54a776b5202874f2e05eb416d35cb2d9ab84
Sun Ning <classicning@gmail.com>
2012-02-28 21:46:05 +0800

    remove gloss dependency, I have to impl it by myself

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject link "0.1.0-SNASHOT"
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [io.netty/netty "3.3.1.Final"]
-                 [gloss "0.2.1-alpha2-SNAPSHOT"]])
+                 [io.netty/netty "3.3.1.Final"]])
 
