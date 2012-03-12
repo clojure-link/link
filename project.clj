6ee18d25dfd603f81fa05b27c69b9a6f9dea9cf0
Sun Ning <classicning@gmail.com>
2012-03-12 21:41:01 +0800

    add object pool infrastructure

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [io.netty/netty "3.3.1.Final"]
-                 [org.clojure/core.match "0.2.0-alpha9"]])
+                 [org.clojure/core.match "0.2.0-alpha9"]
+                 [commons-pool "1.5.6"]])
 
---
+++ src/link/pool.clj
@@ -0,0 +1,32 @@
+(ns link.pool
+  (:import [org.apache.commons.pool PoolableObjectFactory])
+  (:import [org.apache.commons.pool.impl GenericObjectPool]))
+
+(defn pool-factory [& {:keys [close-fn open-fn validate-fn]
+                       :or {close-fn (fn [_])
+                            open-fn (fn [] nil)
+                            validate-fn (fn [_] true)}}]
+  (reify PoolableObjectFactory
+    (makeObject [this]
+      (open-fn))
+    (destroyObject [this obj]
+      (close-fn obj))
+    (validateObject [this obj]
+      (validate-fn obj))
+    (activateObject [this obj]
+      )
+    (passivateObject [this obj]
+      )))
+
+(def exhausted-actions
+  {:fail GenericObjectPool/WHEN_EXHAUSTED_FAIL
+   :block GenericObjectPool/WHEN_EXHAUSTED_BLOCK
+   :grow GenericObjectPool/WHEN_EXHAUSTED_GROW})
+
+(defn create-pool [factory max-active exhausted-action max-wait max-idle]
+  (GenericObjectPool. factory
+                      max-active
+                      (exhausted-actions exhausted-actions)
+                      max-wait
+                      max-idle))
+
