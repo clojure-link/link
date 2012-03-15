559f125ee1b136d761bca1345dec90ed93ef93b5
Sunng <classicning@gmail.com>
2012-03-15 13:16:49 +0800

    netty http request object to ring request map

--- src/link/http.clj
+++ src/link/http.clj
@@ -1,14 +1,23 @@
 (ns link.http
   (:use [link.core])
+  (:use [clojure.string :only [lower-case]])
   (:import [java.net InetSocketAddress])
   (:import [java.util.concurrent Executors])
   (:import [org.jboss.netty.channel
+            Channel
             Channels
-            ChannelPipelineFactory])
+            ChannelPipelineFactory
+            MessageEvent])
+  (:import [org.jboss.netty.buffer
+            ChannelBufferInputStream])
   (:import [org.jboss.netty.bootstrap ServerBootstrap])
   (:import [org.jboss.netty.channel.socket.nio
             NioServerSocketChannelFactory])
   (:import [org.jboss.netty.handler.codec.http
+            HttpRequest
+            HttpHeaders
+            HttpHeaders$Names
+            DefaultHttpResponse
             HttpRequestDecoder
             HttpResponseEncoder]))
 
@@ -19,6 +28,23 @@
         (.addLast pipeline "decoder" (HttpRequestDecoder.))
         (.addLast pipeline "encoder" (HttpResponseEncoder.))
         (.addLast pipeline "handler" handler)))))
+
+(defn ring-request [^Channel c ^MessageEvent e]
+  (let [server-addr (.getLocalAddress c)
+        addr (.getRemoteAddress e)
+        req (.getMessage e)]
+    {:server-addr (.getHostName server-addr)
+     :server-port (.getPort server-addr)
+     :remote-addr (.getHostName addr)
+     :uri (.getUri req)
+     :query-string (subs (.getUri req) (+ 1 (.indexOf (.getUri req) \?)))
+     :scheme :http
+     :request-method (keyword (lower-case (.. req getMethod getName)))
+     :content-type (HttpHeaders/getHeader req HttpHeaders$Names/CONTENT_TYPE)
+     :content-length (HttpHeaders/getContentLength req)
+     :character-encoding (HttpHeaders/getHeader req HttpHeaders$Names/CONTENT_ENCODING)
+     :headers (as-map (.getHeaders req))
+     :body (ChannelBufferInputStream. (.getContent req))}))
 
 (defn create-http-handler-from-ring [ring-fn]
   (create-handler
