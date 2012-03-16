1dac7661719564130bc067350b2a54136bdf1bdd
Sunng <classicning@gmail.com>
2012-03-16 13:57:23 +0800

    fix typo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link "0.1.0-SNASHOT"
+(defproject link "0.1.0-SNAPSHOT"
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [io.netty/netty "3.3.1.Final"]
