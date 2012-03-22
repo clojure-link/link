3d4b91b14f92dbef63e1f3003c651ad02be45a5e
Sun Ning <classicning@gmail.com>
2012-03-22 19:58:36 +0800

    remove pool

--- src/link/tcp.clj
+++ src/link/tcp.clj
@@ -1,6 +1,5 @@
 (ns link.tcp
   (:use [link.codec :only [netty-encoder netty-decoder]])
-  (:use [link.pool :only [pool-factory create-pool]])
   (:import [java.net InetSocketAddress])
   (:import [java.util.concurrent Executors])
   (:import [org.jboss.netty.bootstrap
@@ -66,28 +65,5 @@
         awaitUninterruptibly
         getChannel)))
 
-(defn tcp-client-pool [host port handler
-                       & {:keys [encoder decoder codec boss-pool worker-pool
-                                 max-active exhausted-action max-wait max-idle]
-                          :or {encoder nil
-                               decoder nil
-                               codec nil
-                               boss-pool (Executors/newCachedThreadPool)
-                               worker-pool (Executors/newCachedThreadPool)
-                               max-active 8
-                               exhausted-action :block
-                               max-wait -1
-                               max-idle 8}}]
-  (let [open-fn (fn [] (tcp-client host port handler
-                                  :encoder encoder
-                                  :decoder decoder
-                                  :codec codec
-                                  :boss-pool boss-pool
-                                  :worker-pool worker-pool))
-        close-fn (fn [c] (.close c))
-        validate-fn (fn [c] (.isOpen c))
-        factory (pool-factory :close-fn close-fn
-                              :open-fn open-fn
-                              :validate-fn validate-fn)]
-    (create-pool factory max-active exhausted-action max-wait max-idle)))
+
 
