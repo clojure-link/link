aad776d8a6ea470d1c602818262911a60536d3a6
Sun Ning <classicning@gmail.com>
2012-04-17 20:20:30 +0800

    bump version 0.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject link "0.3.0-SNAPSHOT"
+(defproject link "0.3.0"
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [io.netty/netty "3.3.1.Final"]]
-  :warn-on-reflection true)
+  :warn-on-reflection false)
 
