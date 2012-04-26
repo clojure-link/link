fd8825792118196667358dcf92c8f858b86de928
Sunng <classicning@gmail.com>
2012-04-26 14:17:45 +0800

    start 0.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject link "0.3.2"
+(defproject link "0.3.2-SNAPSHOT"
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [io.netty/netty "3.3.1.Final"]]
+                 [io.netty/netty "3.4.1.Final"]]
   :warn-on-reflection false)
 
