68af2a7c8732fa40acc48ec771ded4c452c7726c
Sun Ning <nsun@avos.com>
2012-06-10 16:31:54 +0800

    update netty

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [io.netty/netty "3.4.6.Final"]
+                 [io.netty/netty "3.5.0.Final"]
                  [commons-pool "1.6"]]
   :warn-on-reflection false)
 
