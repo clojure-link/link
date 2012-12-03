898fb26618a6e4e519af09729761cee7c55ac082
Paul Dumais <paul@unstate.ca>
2012-12-02 20:42:42 -0700

    changed project name for fork

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject link "0.3.3"
+(defproject org.clojars.pauld/link "0.3.4-SNAPSHOT"
   :description
-  "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
+  "A straightforward (not-so-clojure) clojure wrapper for java nio framework.
+   This fork adds TLS support."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [io.netty/netty "3.5.7.Final"]]
   :warn-on-reflection false)
