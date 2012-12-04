0abdef4dee679829f50f45a342c8e21b5d76f19a
Paul Dumais <paul@unstate.ca>
2012-12-03 23:41:06 -0700

    undid some things to make this suitable for pull request.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.clojars.pauld/link "0.3.4-SNAPSHOT"
+(defproject link "0.3.3"
   :description
-  "A straightforward (not-so-clojure) clojure wrapper for java nio framework.
-   This fork adds TLS support."
+  "A straightforward (not-so-clojure) clojure wrapper for java nio framework."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [io.netty/netty "3.5.7.Final"]]
   :warn-on-reflection false)
