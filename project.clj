56615437178ee0dc28a69c330abab1959aebcba1
Paul Dumais <paul@unstate.ca>
2013-01-19 18:50:02 -0700

    added to project description

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.pauld/link "0.4.0-SNAPSHOT"
   :description
   "A straightforward (not-so-clojure) clojure wrapper for java nio framework.
-   This fork provides tls support."
+   This fork provides tls support. Now with tls and server shutdown."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [io.netty/netty "3.5.7.Final"]]
   :warn-on-reflection false)
