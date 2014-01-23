d13f82fd8b0b382ef9f2de5c112264ee6ecc780e
Sun Ning <nsun@avos.com>
2014-01-23 14:41:26 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/sunng87/radar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [io.netty/netty "3.6.1.Final"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.netty/netty-all "4.0.15.Final"]
                  [commons-pool "1.6"]])
-
