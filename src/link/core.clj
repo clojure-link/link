9c4235237b41b75cfb7e163db72c47938592cfbf
Sun Ning <nsun@avos.com>
2014-01-24 17:26:59 +0800

    port channelHandler to Netty 4 and pass test

--- src/link/core.clj
+++ src/link/core.clj
@@ -3,17 +3,10 @@
   (:import [clojure.lang IDeref])
   (:import [java.net InetSocketAddress])
   (:import [java.nio.channels ClosedChannelException])
-  (:import [org.jboss.netty.channel
+  (:import [io.netty.channel
             Channel
             ChannelHandlerContext
-            MessageEvent
-            ExceptionEvent
-            WriteCompletionEvent
-            SimpleChannelUpstreamHandler])
-  (:import [org.jboss.netty.handler.execution
-            ExecutionHandler
-            MemoryAwareThreadPoolExecutor
-            OrderedMemoryAwareThreadPoolExecutor]))
+            SimpleChannelInboundHandler]))
 
 (defprotocol MessageChannel
   (send [this msg])
@@ -23,10 +16,7 @@
   (close [this]))
 
 (defn- client-channel-valid? [^Channel ch]
-  (and (not (nil? ch))
-       (.isOpen ch)
-       (.isBound ch)
-       (.isConnected ch)))
+  (and ch (.isActive ch)))
 
 (deftype ClientSocketChannel [ch-agent factory-fn]
   MessageChannel
@@ -35,12 +25,12 @@
                        (fn [ch]
                          (let [valid (client-channel-valid? ch)
                                ch- (if valid ch (factory-fn))]
-                           (.write ^Channel ch- msg)
+                           (.writeAndFlush ^Channel ch- msg)
                            ch-))))
   (channel-addr [this]
-    (.getLocalAddress ^Channel @ch-agent))
+    (.localAddress ^Channel @ch-agent))
   (remote-addr [this]
-    (.getRemoteAddress ^Channel @ch-agent))
+    (.remoteAddress ^Channel @ch-agent))
   (close [this]
     (clojure.core/send ch-agent
                        (fn [ch]
@@ -54,13 +44,13 @@
   (send [this msg]
     (.write ch msg))
   (channel-addr [this]
-    (.getLocalAddress ch))
+    (.localAddress ch))
   (remote-addr [this]
-    (.getRemoteAddress ch))
+    (.remoteAddress ch))
   (close [this]
     (.close ch))
   (valid? [this]
-    (and (not (nil? ch)) (.isOpen ch) (.isBound ch))))
+    (and ch (.isActive ch))))
 
 (defmacro ^{:private true} make-handler-macro [evt]
   (let [handler-name (str "on-" evt)
@@ -70,69 +60,33 @@
     `(defmacro ~symbol-name [~args-vec-sym & ~body-sym]
        `{(keyword ~~handler-name) (fn ~~args-vec-sym ~@~body-sym)})))
 
-(make-handler-macro open)
-(make-handler-macro close)
 (make-handler-macro message)
 (make-handler-macro error)
-(make-handler-macro connected)
-(make-handler-macro disconnected)
-(make-handler-macro write-complete)
+(make-handler-macro active)
+(make-handler-macro inactive)
 
 (defmacro create-handler [& body]
   `(let [handlers# (merge ~@body)]
-     (proxy [SimpleChannelUpstreamHandler] []
-       (channelClosed [^ChannelHandlerContext ctx# e#]
-         (if-let [handler# (:on-close handlers#)]
-           (handler# (SimpleWrappedSocketChannel. (.getChannel ctx#)))
-           (.sendUpstream ctx# e#)))
-       (channelConnected [^ChannelHandlerContext ctx# e#]
-         (if-let [handler# (:on-connected handlers#)]
-           (handler# (SimpleWrappedSocketChannel. (.getChannel ctx#)))
-           (.sendUpstream ctx# e#)))
-       (channelDisconnected [^ChannelHandlerContext ctx# e#]
-         (if-let [handler# (:on-disconnected handlers#)]
-           (handler# (SimpleWrappedSocketChannel. (.getChannel ctx#)))
-           (.sendUpstream ctx# e#)))
-       (channelOpen [^ChannelHandlerContext ctx# e#]
-         (if-let [handler# (:on-open handlers#)]
-           (handler# (SimpleWrappedSocketChannel. (.getChannel ctx#)))
-           (.sendUpstream ctx# e#)))
-       
+     (proxy [SimpleChannelInboundHandler] []
+       (channelActive [^ChannelHandlerContext ctx#]
+         (when-let [handler# (:on-active handlers#)]
+           (handler# (SimpleWrappedSocketChannel. (.channel ctx#))))
+         (.fireChannelActive ctx#))
+
+       (channelInactive [^ChannelHandlerContext ctx#]
+         (when-let [handler# (:on-inactive handlers#)]
+           (handler# (SimpleWrappedSocketChannel. (.channel ctx#))))
+         (.fireChannelInactive ctx#))
+
        (exceptionCaught [^ChannelHandlerContext ctx#
-                         ^ExceptionEvent e#]
+                         ^Throwable e#]
          (when-let [handler# (:on-error handlers#)]
-           (let [ch# (SimpleWrappedSocketChannel. (.getChannel ctx#))
-                 exp# (.getCause e#)]
-             (handler# ch# exp#)))
-         (.sendUpstream  ctx# e#))
-       
-       (messageReceived [^ChannelHandlerContext ctx#
-                         ^MessageEvent e#]
+           (let [ch# (SimpleWrappedSocketChannel. (.channel ctx#))]
+             (handler# ch# e#)))
+         (.fireExceptionCaught  ctx# e#))
+
+       (channelRead0 [^ChannelHandlerContext ctx# msg#]
          (when-let [handler# (:on-message handlers#)]
-           (let [message# (.getMessage e#)
-                 addr# (.getRemoteAddress e#)
-                 ch# (SimpleWrappedSocketChannel. (.getChannel ctx#))]
-             (handler# ch# message# addr#)))
-         (.sendUpstream ctx# e#))
-
-       (writeComplete [^ChannelHandlerContext ctx#
-                       ^WriteCompletionEvent e#]
-         (when-let [handler# (:on-write-complete handlers#)]
-           (let [amount# (.getWrittenAmount e#)
-                 ch# (SimpleWrappedSocketChannel. (.getChannel ctx#))]
-             (handler# ch# amount#)))
-         (.sendUpstream ctx# e#)))))
-
-(defn threaded-handler [ordered]
-  (let [core-size 20
-        max-channel-memory 0 ;;unlimited
-        max-total-memory 0 ;;unlimited
-        ]
-   (ExecutionHandler.
-    (if ordered
-      (OrderedMemoryAwareThreadPoolExecutor.
-       core-size max-channel-memory max-total-memory)
-      (MemoryAwareThreadPoolExecutor.
-       core-size max-channel-memory max-total-memory)))))
-
-
+           (let [ch# (SimpleWrappedSocketChannel. (.channel ctx#))]
+             (handler# ch# msg#)))
+         (.fireChannelRead ctx# msg#)))))
--- test/link/test/core.clj
+++ test/link/test/core.clj
@@ -3,50 +3,27 @@
   (:use [link.core])
   (:use [clojure.test])
   (:import [java.net InetSocketAddress])
-  (:import [org.jboss.netty.channel
-            ChannelHandlerContext
-            MessageEvent
-            ExceptionEvent]))
+  (:import [io.netty.channel ChannelHandlerContext]))
 
-(def msg-event
-  (reify MessageEvent
-    (getMessage [this] "msg")
-    (getRemoteAddress [this] (InetSocketAddress. "127.0.0.1" 2104))
-    (getChannel [this])
-    (getFuture [this])))
+(def msg "msg")
 
-(def exp-event
-  (reify ExceptionEvent
-    (getCause [this] (Exception.))
-    (getChannel [this])
-    (getFuture [this])))
+(def exp (Exception.))
 
 (def ch-handle-ctx
   (reify ChannelHandlerContext
-    (canHandleDownstream [this])
-    (canHandleUpstream [this])
-    (getAttachment [this])
-    (getChannel [this] nil)
-    (getHandler [this])
-    (getName [this])
-    (getPipeline [this])
-    (sendDownstream [this e])
-    (sendUpstream [this e])
-    (setAttachment [this o])))
+    (channel [this])
+    (fireChannelRead [this msg])
+    (fireChannelActive [this])
+    (fireChannelInactive [this])
+    (fireExceptionCaught [this e])))
 
 (deftest test-handler
   (let [test-handler (create-handler
-                      (on-open [ch] true)
-                      (on-close [ch] true)
-                      (on-message [ch msg addr] true)
-                      (on-error [ch e] true)
-                      (on-connected [ch] true)
-                      (on-disconnected [ch] true))
-        ]
-    (is (nil? (.channelClosed test-handler ch-handle-ctx nil)))
-    (is (nil? (.channelConnected test-handler ch-handle-ctx nil)))
-    (is (nil? (.channelDisconnected test-handler ch-handle-ctx nil)))
-    (is (nil? (.channelOpen test-handler ch-handle-ctx nil)))
-    (is (nil? (.exceptionCaught test-handler ch-handle-ctx exp-event)))
-    (is (nil? (.messageReceived test-handler ch-handle-ctx msg-event)))))
-
+                      (on-active [ch] true)
+                      (on-inactive [ch] true)
+                      (on-message [ch msg] true)
+                      (on-error [ch e] true))]
+    (is (nil? (.channelActive test-handler ch-handle-ctx)))
+    (is (nil? (.channelInactive test-handler ch-handle-ctx)))
+    (is (nil? (.exceptionCaught test-handler ch-handle-ctx exp)))
+    (is (nil? (.channelRead0 test-handler ch-handle-ctx msg)))))
