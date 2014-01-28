6bea6438d23f21809038d4372103088779572c26
Sun Ning <nsun@avos.com>
2014-01-28 17:59:45 +0800

    pom changes

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject link "0.5.0-SNAPSHOT"
   :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :url "http://github.com/sunng87/link"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.netty/netty-all "4.0.15.Final"]]
+  :scm {:name "git"
+        :url "http://github.com/sunng87/link"}
   :global-vars {*warn-on-reflection* true})
