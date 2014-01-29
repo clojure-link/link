ece8ab7236563fd2499ddb7fcdae130ffa26a9b0
Sun Ning <nsun@avos.com>
2014-01-29 15:59:43 +0800

    bump version and release 0.5

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject link "0.5.0-SNAPSHOT"
-  :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
+(defproject link "0.5.0"
+  :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
