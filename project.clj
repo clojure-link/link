e4b2f0f112ee67e89551a42dbf82ce004de3c4a5
Sun Ning <nsun@avos.com>
2014-02-26 13:25:08 +0800

    bump netty version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.netty/netty-all "4.0.15.Final"]]
+                 [io.netty/netty-all "4.0.17.Final"]]
   :scm {:name "git"
         :url "http://github.com/sunng87/link"}
   :global-vars {*warn-on-reflection* true})
