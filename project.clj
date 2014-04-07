ffbf8b07015e8922c773e38b1515a4c10581cf7b
Sun Ning <nsun@avos.com>
2014-04-07 10:24:16 +0800

    update dependecnies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject link "0.6.4"
+(defproject link "0.6.5"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.netty/netty-all "4.0.17.Final"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [io.netty/netty-all "4.0.18.Final"]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}}
   :scm {:name "git"
