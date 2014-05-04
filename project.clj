4fec6d12bc0f5fc5d691dc130cf9d34c1f1420e3
Sun Ning <nsun@avos.com>
2014-05-04 15:30:12 +0800

    update netty to latest release

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.netty/netty-all "4.0.18.Final"]
+                 [io.netty/netty-all "4.0.19.Final"]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}}
   :scm {:name "git"
