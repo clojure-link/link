af18064014f49ea201ff64d63a2817d4751f39be
Sun Ning <nsun@avos.com>
2014-05-09 17:34:00 +0800

    lein-release plugin: preparing 0.6.9 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link "0.6.9-SNAPSHOT"
+(defproject link "0.6.9"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
