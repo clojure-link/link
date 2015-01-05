2444136a371f11a64723ec4db1d496fbc09d9ac0
Sun Ning <sunng@about.me>
2015-01-05 22:05:27 +0800

    update to Netty 4.0.25

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.netty/netty-buffer "4.0.24.Final"]
-                 [io.netty/netty-codec-http "4.0.24.Final"]
-                 [io.netty/netty-codec "4.0.24.Final"]
-                 [io.netty/netty-common "4.0.24.Final"]
-                 [io.netty/netty-handler "4.0.24.Final"]
-                 [io.netty/netty-transport "4.0.24.Final"]
+                 [io.netty/netty-buffer "4.0.25.Final"]
+                 [io.netty/netty-codec-http "4.0.25.Final"]
+                 [io.netty/netty-codec "4.0.25.Final"]
+                 [io.netty/netty-common "4.0.25.Final"]
+                 [io.netty/netty-handler "4.0.25.Final"]
+                 [io.netty/netty-transport "4.0.25.Final"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}}
   :scm {:name "git"
