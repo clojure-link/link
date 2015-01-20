7c00afd8a7fa63774af688fd077953f07b9dd1da
Sun Ning <sunng@about.me>
2015-01-20 15:38:56 +0800

    (feat) added API to support legacy JDK SSLContext

--- src/link/ssl.clj
+++ src/link/ssl.clj
@@ -1,7 +1,12 @@
 (ns link.ssl
   (:import [io.netty.handler.ssl SslHandler SniHandler SslContext]
            [io.netty.channel Channel]
-           [io.netty.util DomainNameMapping]))
+           [io.netty.util DomainNameMapping]
+           [javax.net.ssl SSLContext]))
+
+(defn ssl-handler-from-jdk-ssl-context [^SSLContext ctx client?]
+  (fn [_] (SslHandler. (doto (.createSSLEngine ctx)
+                        (.setUseClientMode client?)))))
 
 (defn ssl-handler [^SslContext context]
   (fn [^Channel ch] (.newHandler context (.alloc ch))))
