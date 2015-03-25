457ee41da272b7ac2369bca8644404746f3c13ee
Sun Ning <sunng@about.me>
2015-03-25 16:56:13 +0800

    (feat) added a default thread factory impl

--- src/link/threads.clj
+++ src/link/threads.clj
@@ -1,5 +1,14 @@
 (ns link.threads
-  (:import [io.netty.util.concurrent DefaultEventExecutorGroup]))
+  (:import [java.util.concurrent ThreadFactory]
+           [io.netty.util.concurrent DefaultEventExecutorGroup]))
 
-(defn new-executor [threads]
-  (DefaultEventExecutorGroup. threads))
+(defn new-executor
+  ([threads] (DefaultEventExecutorGroup. threads))
+  ([threads thread-factory] (DefaultEventExecutorGroup. threads thread-factory)))
+
+(defn prefix-thread-factory [name-prefix]
+  (let [counter (atom 0)]
+    (reify ThreadFactory
+      (newThread [this r]
+        (doto (Thread. r)
+          (.setName (str name-prefix "-" (swap! counter inc))))))))
---
+++ test/link/test/threads.clj
@@ -0,0 +1,9 @@
+(ns link.test.threads
+  (:require [clojure.test :refer :all]
+            [link.threads :refer :all]))
+
+(deftest test-thread-factory
+  (testing "the generated thread name should starts with fixed prefix"
+    (let [tf (prefix-thread-factory "link")
+          t (.newThread tf (fn []))]
+      (is (= "link-1" (.getName t))))))
