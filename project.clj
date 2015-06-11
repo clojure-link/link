653bc83f1ae4a97df2e1f3728227e23e20abeef4
Ning Sun <sunng@about.me>
2015-06-11 17:42:26 +0800

    Version 0.8.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link "0.8.5"
+(defproject link "0.8.6-SNAPSHOT"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
