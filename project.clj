5df6b155f44e386f05a42ed6f04a365aae5ba015
Ning Sun <sunng@about.me>
2015-06-23 15:02:32 +0800

    (chore) update netty to 4.0.28

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
+(def netty-version "4.0.28.Final")
 (defproject link "0.8.6-SNAPSHOT"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.netty/netty-buffer "4.0.26.Final"]
-                 [io.netty/netty-codec-http "4.0.26.Final"]
-                 [io.netty/netty-codec "4.0.26.Final"]
-                 [io.netty/netty-common "4.0.26.Final"]
-                 [io.netty/netty-handler "4.0.26.Final"]
-                 [io.netty/netty-transport "4.0.26.Final"]
+                 [io.netty/netty-buffer ~netty-version]
+                 [io.netty/netty-codec-http ~netty-version]
+                 [io.netty/netty-codec ~netty-version]
+                 [io.netty/netty-common ~netty-version]
+                 [io.netty/netty-handler ~netty-version]
+                 [io.netty/netty-transport ~netty-version]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}}
   :scm {:name "git"
