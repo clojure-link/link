1af9f3c71265f1cac654fa0bf1bb8ee98eb8ea32
Ning Sun <sunng@about.me>
2016-04-01 10:48:30 +0800

    (chore) update netty

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def netty-version "4.0.30.Final")
+(def netty-version "4.0.35.Final")
 (defproject link "0.8.15-SNAPSHOT"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
