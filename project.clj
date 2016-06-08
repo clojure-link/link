d6f7a9502f7a475c3aea7724328020ee11626177
Ning Sun <sunng@about.me>
2016-06-08 11:00:03 +0800

    Version 0.8.20-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def netty-version "4.0.37.Final")
-(defproject link "0.8.19"
+(defproject link "0.8.20-SNAPSHOT"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
   :license {:name "Eclipse Public License - v 1.0"
