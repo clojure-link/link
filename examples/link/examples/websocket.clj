af68a2ee28bea6ac1a470c83be789fc53391f02e
Ning Sun <sunng@about.me>
2016-08-05 14:49:14 +0800

    (feat) support websocket compression
    Signed-off-by: Ning Sun <sunng@about.me>

---
+++ examples/link/examples/websocket.clj
@@ -0,0 +1,18 @@
+(ns link.examples.websocket
+  (:require [link.websocket :refer :all]
+            [link.tcp :refer :all]
+            [link.core :refer [send!]]))
+
+(def handler
+  (create-websocket-handler
+   (on-text [ch msg]
+            (send! ch (text (.alloc ch) msg)))
+   (on-error [ch e]
+             (.printStackTrace e))))
+
+(defn -main [& args]
+  (println ";; Starting websocket server")
+  (tcp-server 8080
+              (conj (websocket-codecs "/")
+                    handler))
+  (println ";; Websocket server started on 8080"))
