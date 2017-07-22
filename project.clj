07e6a8bfdbbfe85bb6568e9097c1d49337ffa5dc
Ning Sun <sunng@about.me>
2017-07-22 10:39:18 +0800

    (feat) update netty
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def netty-version "4.1.12.Final")
+(def netty-version "4.1.13.Final")
 (defproject link "0.10.2-SNAPSHOT"
   :description "A clojure framework for nonblocking network programming"
   :url "http://github.com/sunng87/link"
