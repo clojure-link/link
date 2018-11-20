8a7a155ccd412733e6d35df4ecee85b3423e8e6b
tcdk <458761603@qq.com>
2018-11-20 19:10:37 +0800

    剩下一地的bug，回头再搞

--- src/link/udp.clj
+++ src/link/udp.clj
@@ -3,55 +3,43 @@
             [link.codec :refer [netty-encoder netty-decoder]]
             [clojure.tools.logging :as logging])
   (:import [java.net InetAddress InetSocketAddress]
+           [java.util List]
            [io.netty.bootstrap Bootstrap]
-           [io.netty.channel ChannelOption]
+           [io.netty.buffer ByteBuf]
+           [io.netty.channel.nio NioEventLoopGroup]
+           [io.netty.handler.codec DatagramPacketDecoder MessageToMessageDecoder]
+           [io.netty.channel ChannelInitializer Channel ChannelHandler ChannelFuture
+            ChannelPipeline ChannelOption]
            [io.netty.channel.socket.nio NioDatagramChannel]
+           [io.netty.util.concurrent EventExecutorGroup]
            [link.core ClientSocketChannel]))
+
 
 (defn- append-single-handler->pipeline
   ([^ChannelPipeline pipeline ^String name ^ChannelHandler h]
-   (.addLast pipeline name h))
-  ([^ChannelPipeline pipeline ^EventExecutorGroup executor ^String name ^ChannelHandler h]
-   (.addLast pipeline executor name h)))
+   (.addLast pipeline name h)))
 
 (defn- append-handlers->pipeline
   ([^ChannelPipeline pipeline handlers]
-   (.addLast pipeline ^"[Lio.netty.channel.ChannelHandler;" (into-array ChannelHandler handlers)))
-  ([^ChannelPipeline pipeline ^EventExecutorGroup executor handlers]
-   (.addLast pipeline executor ^"[Lio.netty.channel.ChannelHandler;" (into-array ChannelHandler handlers))))
+    (prn handlers)
+   (.addLast pipeline (into-array ChannelHandler handlers))))
 
-(defn channel-init [handler-specs]
+(defn channel-init [handlers]
   (proxy [ChannelInitializer] []
     (initChannel [^NioDatagramChannel ch]
-      (let [pipeline ^ChannelPipeline (.pipeline ch)
-            ;; the handler-spec itself can be a factory function that
-            ;; returns handler-specs
-            handler-specs (if (fn? handler-specs)
-                            (handler-specs ch)
-                            handler-specs)]
-        (doseq [hs handler-specs]
-          (let [hs (if (map? hs) hs {:handler hs})
-                h (if (fn? (:handler hs)) ((:handler hs) ch) (:handler hs))]
-            (cond
-              (and (:executor hs) (:name hs))
-              (append-single-handler->pipeline pipeline (:executor hs) (:name hs) h)
-
-              (:name hs)
-              (append-single-handler->pipeline pipeline (:name hs) h)
-
-              (:executor hs)
-              (append-handlers->pipeline pipeline (:executor hs) [h])
-
-              :else
-              (append-handlers->pipeline pipeline [h]))))))))
+      (let [pipeline ^ChannelPipeline (.pipeline ch)]
+        (append-single-handler->pipeline pipeline "udp-decoder"
+          (DatagramPacketDecoder.
+            (proxy [MessageToMessageDecoder] []
+              (decode [ctx ^ByteBuf buf ^List out]
+                (.add out buf)))))
+        (doseq [h handlers]
+          (let [h (if (fn? h) (h ch) h)]
+          (append-handlers->pipeline pipeline [h])))))))
 
 (defn- start-udp-server [host port handlers options]
   (let [worker-group (or (:worker-group options) (NioEventLoopGroup.))
         bootstrap (or (:bootstrap options) (Bootstrap.))
-        handlers (cond
-                   (fn? handlers) handlers
-                   (sequential? handlers) handlers
-                   :else [handlers])
         channel-initializer (channel-init handlers)]
 
         (doto bootstrap
@@ -67,11 +55,4 @@
                   & {:keys [options host]
                      :or {options {}
                           host "0.0.0.0"}}]
-  (let [handlers (cond
-                   (fn? handlers) handlers
-                   (sequential? handlers) handlers
-                   :else [handlers])]
-    (start-udp-server host
-                      port
-                      handlers
-                      options)))
+    (start-udp-server host port handlers options))
